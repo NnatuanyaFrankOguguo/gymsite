15cc003199e24095e6521fd6c2c96d97ece3236e
NnatuanyaFrankOguguo <nnatuanyafrank@gmail.com>
2024-10-22 18:59:58 +0100

    ok

--- frontend/src/components/Videosection/Video.jsx
+++ frontend/src/components/Videosection/Video.jsx
@@ -27,54 +27,134 @@ const Video = () => {
 
         {/* CLASS SECTION UNDER THE VIDEO SECTION... */}
 
-        <div className="section class bg-dark has-bg-image" id='class' style={{backgroundImage: `url(${dark_img})`}}>
-            <div className='class-container'>
-                <p className="section-subtitle">Our Classes</p>
+        <div className="section classes bg-dark has-bg-image" id='class' style={{backgroundImage: `url(${dark_img})`}}>
+            <div className='mistake'>
+                <div className='class-container'>
+                    <p className="section-subtitle">Our Classes</p>
 
-                <h2 className="h3 section-title text-center">Fitness Classes For Every Goal</h2>
+                    <h2 className="h3 section-title text-center">Fitness Classes For Every Goal</h2>
 
-            </div>
+                </div>
 
-            <ul className="class-list has-scrollbar">
+                <ul className="class-list has-scrollbar">
 
-                <li className="scrollbar-item">
-                    <div className="class-card">
-                        <figure className="card-banner img-holder" style={{width:'416px', height:'240px'}}>
-                            <img src={img_11} alt="" style={{width: '416px', height:"240px", }} loading='lazy' className='img-cover' />
-                        </figure>
+                    <li className="scrollbar-item">
+                        <div className="class-card">
+                            <figure className="card-banner img-holder" style={{width:'416px', height:'240px'}}>
+                                <img src={img_11} alt="" style={{width: '416px', height:"240px", }} loading='lazy' className='img-cover' />
+                            </figure>
 
-                        <div className="class-content">
-                            <div className="title-wrapper">
-                                <img src={weight_lifting} alt="" style={{width: "52px", height:"52px"}} className='title-icon' />
-                            </div>
+                            <div className="card-content">
+                                <div className="title-wrapper">
+                                    <img src={weight_lifting} alt="" style={{width: "45px", height:"52px"}} className='title-icon' />
 
-                            <h3 className='h3'>Weight Lifting</h3>
-
-                            <p className="card-text">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, fuga perferendis.
-                            </p>
-
-                            <div className="class-progress">
-                                <div className="progress-wrapper">
-                                    <p className="progress-label">
-                                        Class Full
-                                    </p>
-                                    <span className="progress-value">85%</span>
+                                    <h3 className='h3'><a href='#' className='card-title'>Weight Lifting</a></h3>
                                 </div>
 
-                                <div className="progress-bg">
-                                    <div className="progress-bar" style={{width:"85%"}}>
 
+                                <p className="card-text">
+                                    Lorem ipsum dolor sit amet consectetur. 
+                                </p>
+
+                                <div className="class-progress">
+                                    <div className="progress-wrapper">
+                                        <p className="progress-label">
+                                            Class Full
+                                        </p>
+                                        <span className="progress-value">85%</span>
+                                    </div>
+
+                                    <div className="progress-bg">
+                                        <div className="progress-bar" style={{width:"85%"}}>
+
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+
+
                         </div>
+                    </li>
+
+                    <li className="scrollbar-item">
+                        <div className="class-card">
+                            <figure className="card-banner img-holder" style={{width:'416px', height:'240px'}}>
+                                <img src={img_11} alt="" style={{width: '416px', height:"240px", }} loading='lazy' className='img-cover' />
+                            </figure>
+
+                            <div className="card-content">
+                                <div className="title-wrapper">
+                                    <img src={weight_lifting} alt="" style={{width: "45px", height:"52px"}} className='title-icon' />
+
+                                    <h3 className='h3'><a href='#' className='card-title'>Weight Lifting</a></h3>
+                                </div>
 
 
-                    </div>
-                </li>
+                                <p className="card-text">
+                                    Lorem ipsum dolor sit amet consectetur. 
+                                </p>
 
-            </ul>
+                                <div className="class-progress">
+                                    <div className="progress-wrapper">
+                                        <p className="progress-label">
+                                            Class Full
+                                        </p>
+                                        <span className="progress-value">85%</span>
+                                    </div>
+
+                                    <div className="progress-bg">
+                                        <div className="progress-bar" style={{width:"85%"}}>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </li>
+
+                    <li className="scrollbar-item">
+                        <div className="class-card">
+                            <figure className="card-banner img-holder" style={{width:'416px', height:'240px'}}>
+                                <img src={img_11} alt="" style={{width: '416px', height:"240px", }} loading='lazy' className='img-cover' />
+                            </figure>
+
+                            <div className="card-content">
+                                <div className="title-wrapper">
+                                    <img src={weight_lifting} alt="" style={{width: "45px", height:"52px"}} className='title-icon' />
+
+                                    <h3 className='h3'><a href='#' className='card-title'>Weight Lifting</a></h3>
+                                </div>
+
+
+                                <p className="card-text">
+                                    Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, corrupti omnis.
+                                </p>
+
+                                <div className="class-progress">
+                                    <div className="progress-wrapper">
+                                        <p className="progress-label">
+                                            Class Full
+                                        </p>
+                                        <span className="progress-value">55%</span>
+                                    </div>
+
+                                    <div className="progress-bg">
+                                        <div className="progress-bar" style={{width:"85%"}}>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </li>
+
+                </ul>
+
+            </div>
 
         </div>
 
